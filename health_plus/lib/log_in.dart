a3ae97ef45d7a8b2bc4e52b3c12dfa17aea0fb2e
Billy272 <billylaroi@gmail.com>
2024-05-30 08:16:11 +0300

    commit

--- health_plus/lib/log_in.dart
+++ health_plus/lib/log_in.dart
@@ -9,19 +9,40 @@ class LogInScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Log In'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Text('Log In'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: const Text('Log In'),
-            ),
-          ],
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("/health_plus/assets/doctor.jpg"),
+          fit: BoxFit.cover,
+        )),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment:
+                CrossAxisAlignment.center, // Center children horizontally
+            children: <Widget>[
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: const Text('Log In'),
+              ),
+            ],
+          ),
         ),
       ),
     );
