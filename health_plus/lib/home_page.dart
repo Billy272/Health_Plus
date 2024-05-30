b893bde3567beaa1e0d6ee86e3b13e10f1e43ec8
Billy272 <billylaroi@gmail.com>
2024-05-30 21:24:54 +0300

    chore: Add "Find a Location" feature to home page

--- health_plus/lib/home_page.dart
+++ health_plus/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:health_plus/homeTabs/location.dart';
 
 class HomePageScreen extends StatelessWidget {
   const HomePageScreen({super.key});
@@ -7,37 +8,23 @@ class HomePageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Home'),
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage("/health_plus/assets/doctor.jpg"),
-          fit: BoxFit.cover,
-        )),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment:
-                CrossAxisAlignment.center, // Center children horizontally
-            children: <Widget>[
-              const Text(
-                'Welcome to Patient Plus',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/login');
-                },
-                child: const Text('Log Out'),
-              ),
-            ],
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: const Text('Find a Location'),
+              leading: const Icon(Icons.map),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LocationScreen()));
+              },
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
