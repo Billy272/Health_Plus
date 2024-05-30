60c6220be12c36a74bc36fe66b0b82ea885776c6
Billy272 <billylaroi@gmail.com>
2024-05-30 12:02:54 +0300

    commit

--- health_plus/lib/log_in.dart
+++ health_plus/lib/log_in.dart
@@ -6,44 +6,49 @@ class LogInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Log In'),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage("/health_plus/assets/doctor.jpg"),
-          fit: BoxFit.cover,
-        )),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment:
-                CrossAxisAlignment.center, // Center children horizontally
-            children: <Widget>[
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/home');
-                },
-                child: const Text('Log In'),
-              ),
-            ],
+      body: Stack(
+        children: <Widget>[
+          Positioned.fill(
+            child: Image.network(
+              'https://whatsnext.nuance.com/wp-content/uploads/10K-splash.jpg',
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
+          Positioned.fill(
+            child: Container(
+              color: const Color.fromARGB(255, 87, 117, 186).withOpacity(0.5),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // Center children horizontally
+              children: <Widget>[
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  child: const Text('Log In'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
