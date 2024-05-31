66c2e7e31a85708a73eaf9b7e38440948e89d980
Billy272 <billylaroi@gmail.com>
2024-05-31 19:31:10 +0300

    chore: Update home page with improved UI for location and doctor selection

--- health_plus/lib/home_page.dart
+++ health_plus/lib/home_page.dart
@@ -15,91 +15,33 @@ class HomePageScreen extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 57, 89, 158),
         leading: IconButton(
           tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-          icon: const Icon(Icons.menu),
+          icon: const Icon(
+            Icons.menu,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
           onPressed: () {
             Scaffold.of(context).openDrawer();
           },
         ),
-        title: const Text('Home'),
+        title: const Text(
+          'Home',
+          style: TextStyle(
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontSize: 20,
+          ),
+        ),
       ),
-      body: Column(
-        children: <Widget>[
-          const SizedBox(height: 5.0),
-          Card(
-            color: const Color.fromARGB(255, 129, 157, 229),
-            child: ListTile(
-              title: const Text(
-                'Find a Location',
-                style: TextStyle(color: Colors.white, fontSize: 16.0),
-              ),
-              leading: const FaIcon(FontAwesomeIcons.mapLocation,
-                  color: Colors.white),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const LocationScreen()));
-              },
-            ),
-          ),
-          const SizedBox(height: 5.0),
-          Card(
-              color: const Color.fromARGB(255, 87, 117, 186),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Card(
+              color: const Color.fromARGB(255, 129, 157, 229),
               child: ListTile(
                 title: const Text(
-                  'Choose a doctor',
+                  'Find a Location',
                   style: TextStyle(color: Colors.white, fontSize: 16.0),
                 ),
-                leading: const FaIcon(FontAwesomeIcons.userNurse,
-                    color: Colors.white),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const DoctorsScreen()));
-                },
-              )),
-          const SizedBox(height: 5.0),
-          Card(
-              color: const Color.fromARGB(255, 87, 117, 186),
-              child: ListTile(
-                title: const Text(
-                  "Make an appointment",
-                  style: TextStyle(color: Colors.white, fontSize: 16.0),
-                ),
-                leading: const Icon(Icons.add_task, color: Colors.white),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AppointmentScreen()));
-                },
-              )),
-          const SizedBox(height: 5.0),
-          Card(
-            color: const Color.fromARGB(255, 57, 86, 158),
-            child: ListTile(
-                title: const Text(
-                  "ER waiting period",
-                  style: TextStyle(color: Colors.white, fontSize: 16.0),
-                ),
-                leading: const Icon(Icons.calendar_month, color: Colors.white),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const WaitingScreen()));
-                }),
-          ),
-          const SizedBox(height: 5.0),
-          Card(
-              color: const Color.fromARGB(255, 225, 57, 96),
-              child: ListTile(
-                title: const Text(
-                  "Find an urgent care location",
-                  style: TextStyle(color: Colors.white, fontSize: 16.0),
-                ),
-                leading: const FaIcon(FontAwesomeIcons.hospital,
+                leading: const FaIcon(FontAwesomeIcons.mapLocation,
                     color: Colors.white),
                 onTap: () {
                   Navigator.push(
@@ -107,8 +49,77 @@ class HomePageScreen extends StatelessWidget {
                       MaterialPageRoute(
                           builder: (context) => const LocationScreen()));
                 },
-              ))
-        ],
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Card(
+                color: const Color.fromARGB(255, 87, 117, 186),
+                child: ListTile(
+                  title: const Text(
+                    'Choose a doctor',
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                  leading: const FaIcon(FontAwesomeIcons.userNurse,
+                      color: Colors.white),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const DoctorsScreen()));
+                  },
+                )),
+            const SizedBox(height: 10.0),
+            Card(
+                color: const Color.fromARGB(255, 87, 117, 186),
+                child: ListTile(
+                  title: const Text(
+                    "Make an appointment",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                  leading: const Icon(Icons.add_task, color: Colors.white),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AppointmentScreen()));
+                  },
+                )),
+            const SizedBox(height: 5.0),
+            Card(
+              color: const Color.fromARGB(255, 57, 86, 158),
+              child: ListTile(
+                  title: const Text(
+                    "ER waiting period",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                  leading:
+                      const Icon(Icons.calendar_month, color: Colors.white),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const WaitingScreen()));
+                  }),
+            ),
+            const SizedBox(height: 5.0),
+            Card(
+                color: const Color.fromARGB(255, 225, 57, 96),
+                child: ListTile(
+                  title: const Text(
+                    "Find an urgent care location",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                  leading: const FaIcon(FontAwesomeIcons.hospital,
+                      color: Colors.white),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LocationScreen()));
+                  },
+                )),
+          ],
+        ),
       ),
     );
   }
