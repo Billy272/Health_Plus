c4afb5ec785b5e7ad35e0881d759798f7113418c
Billy272 <billylaroi@gmail.com>
2024-05-31 10:48:07 +0300

    chore: Update chat screen with options to chat with different healthcare professionals

--- health_plus/lib/chat.dart
+++ health_plus/lib/chat.dart
@@ -5,6 +5,65 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 0, 64, 255),
+        leading: IconButton(
+          tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+        ),
+        title: const Text('Chat'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: const Text('Chat with a doctor'),
+              minTileHeight: BorderSide.strokeAlignCenter,
+              leading: const Icon(Icons.chat_rounded),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ChatScreen()));
+              },
+            ),
+          ),
+          const SizedBox(height: 5.0),
+          Card(
+              child: ListTile(
+            title: const Text('Chat with a nurse'),
+            leading: const Icon(Icons.chat_rounded),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const ChatScreen()));
+            },
+          )),
+          const SizedBox(height: 5.0),
+          Card(
+              child: ListTile(
+            title: const Text('Chat with a pharmacist'),
+            leading: const Icon(Icons.chat_rounded),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const ChatScreen()));
+            },
+          )),
+          const SizedBox(height: 5.0),
+          Card(
+              child: ListTile(
+            title: const Text('Chat with a therapist'),
+            leading: const Icon(Icons.chat_rounded),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const ChatScreen()));
+            },
+          )),
+        ],
+      ),
+    );
   }
 }
