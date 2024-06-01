332c321c57239740bdbf5c0e14feb03e48aef39b
Billy272 <billylaroi@gmail.com>
2024-06-01 21:42:59 +0300

    chore: Update VSCode settings, pubspec.yaml, AndroidManifest.xml, build.gradle, and location.dart

--- health_plus/lib/homeTabs/location.dart
+++ health_plus/lib/homeTabs/location.dart
@@ -1,37 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class LocationScreen extends StatelessWidget {
+class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
+
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-0.2, 30.80);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 57, 89, 158),
-        title: const Row(
-          children: <Widget>[
-            Spacer(flex: 2),
-            Text(
-              'Location',
-              style: TextStyle(
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 57, 89, 158),
+          title: const Row(
+            children: <Widget>[
+              Spacer(flex: 2),
+              Text(
+                'Location',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Spacer(flex: 3),
-          ],
-        ),
-      ),
-      body: const Center(
-        child: Text(
-          'Location',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+              Spacer(flex: 3),
+            ],
           ),
         ),
-      ),
-    );
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ));
   }
 }
